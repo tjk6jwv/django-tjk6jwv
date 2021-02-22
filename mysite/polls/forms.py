612d436245c38a34e0557b031a4c4fbc51268d37
Thomas Keathley <tjk6jwv@virginia.edu>
2021-02-22 07:54:10 -0500

    Completed bootstrap integration

--- mysite/polls/forms.py
+++ mysite/polls/forms.py
@@ -6,6 +6,18 @@ class ThoughtForm(forms.ModelForm):
     class Meta:
         model = Thought
         fields = ['title', 'thought']
+        widgets = {
+            'title': forms.TextInput(
+				attrs={
+					'class': 'form-control'
+					}
+				),
+            'thought': forms.Textarea(
+				attrs={
+					'class': 'form-control'
+					}
+				),
+			}
     # title_text = forms.CharField(max_length=200)
     # thought_text = forms.CharField(max_length=1000)
     # pub_date = forms.DateTimeField()
